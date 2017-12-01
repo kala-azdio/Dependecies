9c0971b3e805b6253af71b1b9522ea4257a664ac
Kaloyan Chorlev <kaloyan.chorlev@azdio.com>
2017-12-01 12:42:24 +0200

    Test2 amendments

--- Test/src/Test2.java
+++ Test/src/Test2.java
@@ -1,11 +1,8 @@
 
 public class Test2 {
-<<<<<<< HEAD
-	
+
 	public static void main(String[] args) {
 		System.out.print("Test2");
 	}
-=======
->>>>>>> branch 'master2' of https://github.com/kala-azdio/Dependecies
 
 }
